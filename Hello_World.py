98cd713c7dacc983867491735727ab147ea800c2
Roel Vugts <roel.vugts@yahoo.com>
2022-09-09 09:23:14 +0200

    hoi wereld

--- Hello_World.py
+++ Hello_World.py
@@ -1,4 +1,8 @@
 
 name = input("Enter your name here: ");
 
-print("Hello " + name);
+print("\nHello " + name);
+print("\nHere are 3 reasons why Git is useful:");
+print("1. It stores versions so you can keep track of changes");
+print("2. It saves your work online");
+print("3. Others can access and change your code");
