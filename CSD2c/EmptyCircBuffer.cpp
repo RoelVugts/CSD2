386799b1ba15bfadfb3bf5aca349ec3690319b18
Roel Vugts <roel.vugts@yahoo.com>
2023-02-07 20:57:40 +0100

    Buffer working

--- CSD2c/EmptyCircBuffer.cpp
+++ CSD2c/EmptyCircBuffer.cpp
@@ -1,36 +1,120 @@
 #include "EmptyCircBuffer.h"
 
-void CircBuffer::input (float value) {
+#include <iostream>
+#include <unistd.h>
+#include <thread>
+#include <cmath>
+
+void CircBuffer::input (float value) 
+{
     buffer[writeHead] = value;
 }
-float CircBuffer::output() {
-    return buffer[readHead];
+
+float CircBuffer::output() 
+{
+    if (readHead > 0)
+        return buffer[readHead];
+    else
+        return 0.0f;
 }
 
-void CircBuffer::setDistance (uint distance) {
-    readHead = writeHead - distance;
+void CircBuffer::setDistance (uint distance) 
+{   
+    if (readHead > distance)
+    {
+        readHead = currentSize - distance;
+    } else
+    {
+        readHead = currentSize - distance + readHead; //wrap readHead to end if it's in front of writeHead
+    }
 }
 
-void CircBuffer::incrementHeads() {
+void CircBuffer::goToDistance(uint distance, int time)
+{
+    int delta = distance - calculateDistance(); //calculate difference between new distance and old distance
+
+    for (int i = 0; i < abs(delta); i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+
+        if (delta > 0)
+            readHead -= 1;
+        else
+            readHead += 1;
+    }
+}
+
+void CircBuffer::setDistance(uint distance, int time)
+{
+    t = std::thread(&CircBuffer::goToDistance, this, distance, time);
+}
+
+int CircBuffer::calculateDistance()
+{
+    if (writeHead < readHead)
+        return currentSize - readHead + writeHead;
+    else
+        return (writeHead - readHead);
+}
+
+void CircBuffer::incrementHeads() 
+{
     incrementWrite();
     incrementRead();
 }
 
-inline void CircBuffer::wrapHeader (uint& head) {
-    if (head > currentSize)
+int CircBuffer::getSize() const
+{
+    return currentSize;
+}
+
+int CircBuffer::getReadPosition() const
+{
+    return readHead;
+}
+
+int CircBuffer::getWritePosition() const
+{
+    return writeHead;
+}
+
+inline void CircBuffer::wrapHeader (uint& head) 
+{
+    if (head >= currentSize)
         head -= currentSize;
-}
-inline void CircBuffer::incrementWrite() {
-    wrapHeader(writeHead++);
-}
-inline void CircBuffer::incrementRead() {
-    wrapHeader(readHead++);
+    else if (head < 0)
+    {
+        head = currentSize + head;
+    }
 }
 
-void CircBuffer::deleteBuffer() {
-    
-    // for (buf : buffer)
-    //     buffer[buf] = 0.0f;
+inline void CircBuffer::incrementWrite() 
+{
+    writeHead++;
+    wrapHeader(writeHead);
+}
+inline void CircBuffer::incrementRead() 
+{
+    readHead++;
+    wrapHeader(readHead);
+}
 
-        delete buffer;
+void CircBuffer::deleteBuffer() 
+{
+        delete[] buffer;
+}
+
+void CircBuffer::setSize(uint size)
+{
+    float* newBuffer = new float[size];
+    std::copy(buffer, buffer+currentSize, newBuffer);
+
+    deleteBuffer();
+
+    buffer = newBuffer;
+
+    if (writeHead < readHead)
+        readHead += (size - currentSize); //reset the distance when buffer is resized
+
+    currentSize = size;
 }
--- CSD2c/EmptyCircBuffer.h
+++ CSD2c/EmptyCircBuffer.h
@@ -2,51 +2,40 @@
 // Created by Dean on 05/02/2023.
 //
 
+#include <iostream>
+#include <thread>
+
 #pragma once
 
 class CircBuffer {
     using uint = unsigned int;
 
 public:
-    CircBuffer (uint size) : currentSize (size), buffer (new float[currentSize]) {
+    CircBuffer (uint size) : currentSize (size), buffer (new float[currentSize]) {  }
+    ~CircBuffer() { deleteBuffer(); std::cout << "buffer deleted" << std::endl; }
 
-    }
-    ~CircBuffer() {
-        deleteBuffer();
-    }
+    void input (float value);
+    float output();
+    void setDistance (uint distance);
+    void goToDistance(uint distance, int time);
+    void setDistance(uint distance, int time);
+    void incrementHeads();
+    void setSize(uint size);
 
-
-    void input (float value) {
-    
-    }
-    float output() {
-    
-    }
-
-    void setDistance (uint distance) {
-    
-    }
-
-    void incrementHeads() {
-
-    }
+    int getSize() const;
+    int getReadPosition() const;
+    int getWritePosition() const;    
+    std::thread t;
 
 private:
-    inline void wrapHeader (uint& head) {
-
-    }
-    inline void incrementWrite() {
-       
-    }
-    inline void incrementRead() {
-       
-    }
-
-    void deleteBuffer() {
-
-    }
+    inline void wrapHeader (uint& head);
+    inline void incrementWrite();
+    inline void incrementRead();
+    void deleteBuffer();
+    int calculateDistance();
 
     float* buffer;
+    float smoothOutput;
     uint currentSize { 0 };
     uint writeHead { 0 }, readHead { 0 };
 };
